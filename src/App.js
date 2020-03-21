0f83b203c2bfabdb39bec22b69cdd896b6cca8af
Benjamin Earley <benjaminearley@MacBook-Pro.local>
2020-03-21 13:28:05 -0400

    Created Tabs for components, created routes with NavLinks. Tabs work, components render, routes work.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header.js";
+import Tabs from "./components/Tabs";
 
 export default function App() {
   return (
     <main data-testid='app'>
       <Header />
+      <Tabs />
     </main>
   );
 }
---
+++ src/components/Tabs.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Button } from "semantic-ui-react"
+import { NavLink, Route } from "react-router-dom"
+
+import WelcomePage from "./WelcomePage"
+import CharacterList from "./CharacterList"
+
+export default function Tabs() {
+    return (
+        <div className="navbar grid-view">
+            <Button.Group widths="4" className="navbar">
+                <Button as={NavLink} exact to="/" activeClassName="activeNavButton">
+                    Home
+                </Button>
+                <Button as={NavLink} to="/characters" activeClassName="activeNavButton">
+                    Characters
+                </Button>
+            </Button.Group>
+
+            <Route exact path="/" component={WelcomePage} />
+            <Route path="/characters" component={CharacterList} />
+        </div>
+    )
+}
